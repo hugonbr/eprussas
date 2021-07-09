6ad43d8e2027ed894fc617655c2ec2b4e36884b7
Hugo Régis <hugonbr@gmail.com>
2021-07-09 10:55:41 -0300

    idade

---
+++ 1_LogicaDeProgramacao/2_Javascript_C/3_Idade_IF_Jovem/myScript.js
@@ -0,0 +1,34 @@
+let data_nasc, data_atual, idade;
+let r;
+
+input1.max = new Date().toISOString().split("T")[0];
+
+//
+function myFunction() {
+    data_nasc = document.getElementById("input1").value;
+
+    //idade = Math.floor((new Date() - new Date(data_nasc).getTime()) / 3.15576e+10);
+    idade = Math.floor((new Date() - new Date(data_nasc)) / 3.15576e+10);
+
+    //criança 0 a 12
+    //jovem 13 a 21
+    //qualquer outra idade else
+
+    if (idade >= 0 && idade < 13 /*<= 12*/) {
+        r = "É criança.";
+    } else if (idade > 12 && idade < 22 /*<= 21*/) {
+        r = "É jovem.";
+    } else {
+        if (idade < 0) {
+            r = "Idade inválida."
+            idade = "";
+            console.log("Idade inválida.");
+        } else {
+            r = "A sua idade está fora da faixa etária.";
+            console.log("Você tá véio");
+        }
+    }
+
+    document.getElementById("r").innerHTML = "R: " + idade + ". " + r;
+}
+
