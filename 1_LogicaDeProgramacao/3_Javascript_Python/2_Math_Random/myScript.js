1e215ad153a63b8dc140aeb410d0ba8f89722680
Hugo Régis <hugonbr@gmail.com>
2021-07-10 22:36:35 -0300

    random

---
+++ 1_LogicaDeProgramacao/3_Javascript_Python/2_Math_Random/myScript.js
@@ -0,0 +1,13 @@
+let min, max, r;
+
+function myFunction() {
+    min = Number(document.getElementById("inputMin").value);
+    max = Number(document.getElementById("inputMax").value);
+
+    if (min >= max) {
+        document.getElementById("r").innerHTML = "Erro, dados inválidos, verifique os valores mínimos e máximos.";
+    } else {
+        r = Math.floor(Math.random() * (max - min + 1)) + min;
+        document.getElementById("r").innerHTML = "MIN: " + min + ", MAX: " + max + " -> " + r;
+    }
+}
