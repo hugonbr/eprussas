2504c50024e298f7eaef19bd7d668748ff56c6ce
Hugo Régis <hugonbr@gmail.com>
2021-08-31 16:46:38 -0300

    mod

--- 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte1/4_Média2Notas/myScript.js
+++ 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte1/4_Média2Notas/myScript.js
@@ -1,10 +1,15 @@
 let n1, n2, m;
 
-function myFunction() {
+function calcular_media() {
     n1 = Number(document.getElementById("input1").value);
     n2 = Number(document.getElementById("input2").value);
     m = (n1 + n2) / 2;
-    m = m.toFixed(2);
+    m = m.toFixed(1);
     document.getElementById("r").innerHTML = "Média: " + m;
 }
 
+function calcular_media2() {
+    n1 = Number(document.getElementById("input1").value);
+    n2 = Number(document.getElementById("input2").value);
+    document.getElementById("r").innerHTML = "Média: " + ((n1 + n2) / 2).toFixed(7);
+}
