9a2def895c26370b91b642138cb02265842ecd1e
Hugo Régis <hugonbr@gmail.com>
2021-09-03 10:07:14 -0300

    mod

--- 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte1/2_Emoji_buttons/s.js
+++ 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte1/2_Emoji_buttons/s.js
@@ -3,8 +3,15 @@ function feliz() {
     document.getElementById('p1').innerHTML =
         "😃";
 }
+
 function triste() {
 
     document.getElementById('p1').innerHTML =
         "😞";
+}
+
+function nao_sei() {
+
+    document.getElementById('p1').innerHTML =
+        "😐";
 }
