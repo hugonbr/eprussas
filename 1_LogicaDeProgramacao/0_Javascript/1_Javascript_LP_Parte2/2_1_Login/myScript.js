6d1a825d0f085baccda0e947538f945fc8f21a56
Hugo Régis <hugonbr@gmail.com>
2021-09-09 15:24:28 -0300

    mod js login

--- 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte2/2_1_Login/myScript.js
+++ 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte2/2_1_Login/myScript.js
@@ -4,7 +4,7 @@ function entrar() {
     login = document.getElementById("input1").value;
     senha = document.getElementById("input2").value;
     if (login === "admin" && senha === "123") {
-        document.getElementById("home").innerHTML = "<h1>Bem-vindo Admin</h1>";
+        document.getElementById("home").innerHTML = "<h3>Bem-vindo Admin</h3>";
     } else {
         alert("login ou senha incorretos!!! Tente novamente.");
     }
