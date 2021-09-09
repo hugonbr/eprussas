eaca206689b101ad864f21cc24761b567498f028
Hugo Régis <hugonbr@gmail.com>
2021-09-09 20:16:11 -0300

    test js login

--- 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte2/2_1_Login/myScript.js
+++ 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte2/2_1_Login/myScript.js
@@ -13,7 +13,7 @@ function entrar() {
     senha = document.getElementById("input2").value;
     //if (login === "admin" || senha === "123") {
     if (login === "admin" && senha === "123") {
-        document.getElementById("home").innerHTML = "<h3>Bem-vindo Admin</h3>";
+        document.getElementById("home").innerHTML = "<h2>Bem-vindo Admin</h2>";
     } else {
         alert("login ou senha incorretos!!! Tente novamente.");
     }
