3b618fffe0b2965c5fb5a4fc6d88a3f420445d8d
Hugo Régis <hugonbr@gmail.com>
2021-09-09 10:22:20 -0300

    mod java

---
+++ 1_LogicaDeProgramacao/1_Java/Java_Parte3/1_JOptionPane/src/App.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Olá Mundo!");
+        JOptionPane.showMessageDialog(null, "Fui escrito em Java");
+        System.exit(0);
+    }
+}
