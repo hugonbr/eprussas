e60627e58e73d38264ea683aa05571f487ee3cea
Hugo Régis <hugonbr@gmail.com>
2021-09-09 15:11:42 -0300

    mod js

--- 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte2/1_Maiorde2/myScript.js
+++ 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte2/1_Maiorde2/myScript.js
@@ -5,11 +5,13 @@ function myFunction() {
     n2 = Number(document.getElementById("input2").value);
 
     if (n1 > n2) {
-        r = "n1=" + n1 + " é o maior";
+        r = n1 + " é o maior (n1)";
+    } else if (n2 > n1) {
+        r = n2 + " é o maior (n2)";
     } else if (n1 == n2) {
         r = "Os valores são iguais";
     } else {
-        r = "n2=" + n2 + " é o maior";
+        r = "Um erro ocorreu, verifique os números e tente novamente."
     }
 
     document.getElementById("r").innerHTML = "R: " + r;
