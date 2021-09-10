d8b6aedcd23c67f6018c1b398f99ba3f1270e53e
Hugo Régis <hugonbr@gmail.com>
2021-09-10 16:11:40 -0300

    mod

--- 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte2/6_Impar_Par/myScript.js
+++ 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte2/6_Impar_Par/myScript.js
@@ -3,11 +3,14 @@ let num, r;
 function myFunction() {
     num = document.getElementById("input1").value;
 
+    if (num < 0) {
+        num = num * (-1);
+    }
     if (num % 2 == 1) {
         r = "impar";
     } else {
         r = "par";
     }
 
-    document.getElementById("r").innerHTML = "R: " + num + " é " + r;
+    document.getElementById("r").innerHTML = r;
 }
