92a66e4d998edbbcfd3e243341701c7707655cef
Hugo Régis <hugonbr@gmail.com>
2021-09-11 18:34:13 -0300

    mod p3

--- 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte3/1_IncrementoDecremento/myScript.js
+++ 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte3/1_IncrementoDecremento/myScript.js
@@ -1,12 +1,16 @@
 let x, r;
 
 x = 5;
-r = ++x;
+//r = ++x;
+x++;
+r = x;
 
 document.getElementById("rI").innerHTML = r;
 
 x = 5;
-r = --x;
+//r = --x;
+x--;
+r = x;
 
 document.getElementById("rD").innerHTML = r;
 
