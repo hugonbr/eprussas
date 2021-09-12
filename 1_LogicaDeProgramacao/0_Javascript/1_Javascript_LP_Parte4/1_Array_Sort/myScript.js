db0bc443e22a457f35701ad74e27cb60e9132cba
Hugo Régis <hugonbr@gmail.com>
2021-09-12 00:04:54 -0300

    mod p4

--- 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte4/1_Array_Sort/myScript.js
+++ 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte4/1_Array_Sort/myScript.js
@@ -1,4 +1,5 @@
 const cars = ["Fiat", "Ford", "Volkswagen", "Chevrolet"];
+const letras = ["C", "A", "D", "E", "H", "G", "B", "F"];
 
 document.getElementById("r").innerHTML = cars;
 
@@ -6,8 +7,18 @@ function cres() {
     cars.sort();
     document.getElementById("r").innerHTML = cars;
 }
+function cresLetras() {
+    letras.sort();
+    document.getElementById("rLetras").innerHTML = letras;
+}
 function decres() {
     cars.sort();
     cars.reverse();
     document.getElementById("r").innerHTML = cars;
 }
+function decresLetras() {
+    letras.sort();
+    letras.reverse();
+    document.getElementById("rLetras").innerHTML = letras;
+}
+
