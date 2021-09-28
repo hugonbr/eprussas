9cc78df795a4061ad7dd516c9c0b0722734be7af
Hugo Régis <hugonbr@gmail.com>
2021-09-28 17:03:53 -0300

    mod

--- 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte1/8_Média2Notas_Cond/myScript.js
+++ 1_LogicaDeProgramacao/0_Javascript/1_Javascript_LP_Parte1/8_Média2Notas_Cond/myScript.js
@@ -4,7 +4,7 @@ function myFunction() {
     n1 = Number(document.getElementById("input1").value);
     n2 = Number(document.getElementById("input2").value);
     m = (n1 + n2) / 2;
-    //m = m.toFixed(1);
+    m = m.toFixed(1);
 
     if (m >= 5.5 && m < 6.0) {
         m = 6.0;
