df6db869d7f5f1e14672c6f80f497d7b111d2f1e
Hugo Régis <hugonbr@gmail.com>
2021-10-17 14:53:39 -0300

    mod

--- 0_Javascript/1_Javascript_LP_Parte2/3_Search/script.js
+++ 0_Javascript/1_Javascript_LP_Parte2/3_Search/script.js
@@ -11,6 +11,9 @@ function search() {
     document.getElementById("bing").innerHTML =
         `<a href="https://www.bing.com/search?q=${pesquisa_format}" target="_blank">Bing: ${pesquisa}</a>`;
 
+    document.getElementById("duckduckgo").innerHTML =
+        `<a href="https://duckduckgo.com" target="_blank">Duckgo: em breve</a>`;
+
 }
 
 function limpar() {
@@ -18,4 +21,5 @@ function limpar() {
     document.getElementById("isearch").value = "";
     document.getElementById("google").innerHTML = "<br>";
     document.getElementById("bing").innerHTML = "<br>";
+    document.getElementById("duckduckgo").innerHTML = "<br>";
 }
