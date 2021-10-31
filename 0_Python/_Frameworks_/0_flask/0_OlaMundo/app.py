1875f9fb46c562885f56ef0a9355dd083f718146
Hugo Régis <hugonbr@gmail.com>
2021-10-31 18:15:51 -0300

    mod ola flask

---
+++ 0_Python/_Frameworks_/0_flask/0_OlaMundo/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
+
+'''
+#Execução direta
+python app.py
+#Execução via flask
+export FLASK_APP=app
+export FLASK_ENV=development
+flask run
+'''
