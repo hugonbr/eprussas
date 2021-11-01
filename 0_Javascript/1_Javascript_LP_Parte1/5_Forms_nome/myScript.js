49bd99e3922e6c72d3ff332e28690d646140baab
Hugo Régis <hugonbr@gmail.com>
2021-11-01 08:38:49 -0300

    mod

--- 0_Javascript/1_Javascript_LP_Parte1/5_Forms_nome/myScript.js
+++ 0_Javascript/1_Javascript_LP_Parte1/5_Forms_nome/myScript.js
@@ -6,14 +6,15 @@
 let nome, sobrenome, nomecompleto;
 
 function ok() {
-
-    document.getElementById("p1").innerHTML = "Dados processados: " + document.getElementById("input1").value + " " + document.getElementById("input2").value;
-}
-
-function ok2() {
     nome = document.getElementById("input1").value;
     sobrenome = document.getElementById("input2").value;
     nomecompleto = nome + " " + sobrenome;
     document.getElementById("p1").innerHTML = "Dados processados: " + nomecompleto;
 }
 
+/*
+function ok2() {
+
+    document.getElementById("p1").innerHTML = "Dados processados: " + document.getElementById("input1").value + " " + document.getElementById("input2").value;
+}
+*/
