360413c99f8d4fe237447a545ff3156a94e8d705
Hugo Régis <hugonbr@gmail.com>
2021-11-02 00:33:33 -0300

    mod

--- 0_Javascript/1_Javascript_LP_Parte2/z_10_Math_Random/myScript.js
+++ 0_Javascript/1_Javascript_LP_Parte2/z_10_Math_Random/myScript.js
@@ -6,10 +6,12 @@ function myFunction() {
     imax = document.getElementById("inputMax");
     max = Number(imax.value);
 
-    if (min >= max || (imin.value.length == 0 || imax.value.length == 0)) {
+    if (min >= max || min < 0 || max < 0 || (imin.value.length == 0 || imax.value.length == 0)) {
         document.getElementById("r").innerHTML = "Erro, dados inválidos, verifique os valores mínimos e máximos.";
+        document.getElementById("r2").innerHTML = "";
     } else {
         r = Math.floor(Math.random() * (max - min + 1)) + min;
+        console.log(r);
         document.getElementById("r").innerHTML = "";
         document.getElementById("r2").innerHTML = `MIN: ${min}, MAX: ${max} -> ${r}`;
     }
