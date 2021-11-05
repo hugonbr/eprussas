c895b9c395105e7afbacf183ae1c024bb4039499
Hugo Régis <hugonbr@gmail.com>
2021-11-05 09:36:21 -0300

    mod

--- 0_Javascript/1_Javascript_LP_Parte1/9_FormVendedor/myScript.js
+++ 0_Javascript/1_Javascript_LP_Parte1/9_FormVendedor/myScript.js
@@ -3,14 +3,15 @@ Informe o salário final do vendedor, sabendo que o seu total é
 o salário base somado com 15% do total de vendas (comissão).
 */
 
-let nomev, sbase, tv, sf;
+let nomev, sb, tv, sf, p;
 //
 function myFunction() {
     nomev = document.getElementById("input1").value;
-    sbase = Number(document.getElementById("input2").value);
+    sb = Number(document.getElementById("input2").value);
     tv = Number(document.getElementById("input3").value);
+    p = Number(document.getElementById("input4").value);
     //Salário: 2000; Vendas: 100
-    sf = sbase + (tv * 15 / 100);
+    sf = sb + (tv * p / 100);
     sf = sf.toFixed(2);
 
     document.getElementById("r").innerHTML = "Vendedor: " + nomev;
