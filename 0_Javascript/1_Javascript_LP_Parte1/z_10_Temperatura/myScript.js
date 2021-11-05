3050a83b3713511ece37de733eee534d6f79e885
Hugo Régis <hugonbr@gmail.com>
2021-11-05 09:46:06 -0300

    mod

--- 0_Javascript/1_Javascript_LP_Parte1/z_10_Temperatura/myScript.js
+++ 0_Javascript/1_Javascript_LP_Parte1/z_10_Temperatura/myScript.js
@@ -9,6 +9,7 @@ let c, f;
 function myFunction() {
     c = Number(document.getElementById("input1").value);
     f = (9 * c + 160) / 5;
+    console.log(((c * 9 / 5) + 32));
     f = f.toFixed(2);
 
     document.getElementById("r").innerHTML = "F: " + f + "º";
