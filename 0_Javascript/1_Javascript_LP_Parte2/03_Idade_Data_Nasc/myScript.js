52a9e24cdb09a46ba86f1c53e79744fd85f1af59
Hugo Régis <hugonbr@gmail.com>
2021-11-08 09:14:17 -0300

    mod

--- 0_Javascript/1_Javascript_LP_Parte2/03_Idade_Data_Nasc/myScript.js
+++ 0_Javascript/1_Javascript_LP_Parte2/03_Idade_Data_Nasc/myScript.js
@@ -10,9 +10,9 @@ function myFunction() {
     if (idade < 0) {
         r = " Data inválida, tente novamente.";
     } else if (idade >= 18) {
-        r = "R: " + idade + " anos. " + " O usuário é maior de idade!";
+        r = "R: " + idade + " ano(s). " + " O usuário é maior de idade!";
     } else {
-        r = "R: " + idade + " anos. " + " O usuário é menor de idade!";
+        r = "R: " + idade + " ano(s). " + " O usuário é menor de idade!";
     }
 
     document.getElementById("r").innerHTML = r;
