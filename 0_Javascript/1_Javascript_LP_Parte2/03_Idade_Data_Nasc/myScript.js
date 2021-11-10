70b3268801c5fbb1846dc3767c9ebad72edccfb3
Hugo Régis <hugonbr@gmail.com>
2021-11-10 08:48:47 -0300

    mod

--- 0_Javascript/1_Javascript_LP_Parte2/03_Idade_Data_Nasc/myScript.js
+++ 0_Javascript/1_Javascript_LP_Parte2/03_Idade_Data_Nasc/myScript.js
@@ -13,7 +13,7 @@ function myFunction() {
     //toFixed() arredondamento completo. Ex: 21.8 => 22
     idade = Math.floor((new Date() - new Date(data_nasc)) / 3.15576e+10);
 
-    if (idade < 0) {
+    if (idade < 0 || idade > 150) {
         r = " Data inválida, tente novamente.";
     } else if (idade >= 18) {
         r = "R: " + idade + " ano(s). " + " O usuário é maior de idade!";
