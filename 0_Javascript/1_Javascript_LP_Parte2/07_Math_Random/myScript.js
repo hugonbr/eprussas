94b91b877fffae7704dff61c3ef724423805f829
Hugo Régis <hugonbr@gmail.com>
2021-11-17 14:58:11 -0300

    mod

--- 0_Javascript/1_Javascript_LP_Parte2/07_Math_Random/myScript.js
+++ 0_Javascript/1_Javascript_LP_Parte2/07_Math_Random/myScript.js
@@ -1,15 +1,19 @@
-let imin, min, imax, max, r;
+let min, max, r;
 
 function myFunction() {
-    imin = document.getElementById("inputMin");
-    min = Number(imin.value);
-    imax = document.getElementById("inputMax");
-    max = Number(imax.value);
+    min = document.getElementById("inputMin").value;
+    max = document.getElementById("inputMax").value;
 
-    if (min >= max || min < 0 || max < 0 || (imin.value.length == 0 || imax.value.length == 0)) {
-        document.getElementById("r").innerHTML = "Erro, dados inválidos, verifique os valores mínimos e máximos.";
+    if (min.length == "" || max.length == "") {
+        console.log("Campos vazios");
+    }
+
+    if (min >= max || min < 0 || max < 0 || (min.length == "" || max.length == "")) {
+        document.getElementById("r").innerHTML = "Erro, dados inválidos, verifique os valores mínimo e máximo.";
         document.getElementById("r2").innerHTML = "";
     } else {
+        min = Number(min);
+        max = Number(max);
         r = Math.floor(Math.random() * (max - min + 1)) + min;
         console.log(r);
         document.getElementById("r").innerHTML = "";
--- 0_Javascript/_Projetos_/temp/free_login_templates/login-form-main/js/main.js
+++ 0_Javascript/_Projetos_/temp/free_login_templates/login-form-main/js/main.js
@@ -1,9 +1,29 @@
+let login, senha;
+
+//função do template
 (function ($) {
-
 	"use strict";
-
-
 })(jQuery);
+//fim função do template
 
 console.log("Olá JS");
 
+function entrar() {
+
+	login = document.getElementById("login").value;
+	senha = document.getElementById("senha").value;
+
+	if (login === "admin" && senha === "123") {
+		//alert("Bem-vindo Admin");
+		document.getElementById("aviso").innerHTML =
+			"Bem-vindo!."
+	} else {
+		//alert("login ou senha incorretos!!! Tente novamente.");
+		document.getElementById("aviso").innerHTML =
+			"login ou senha incorretos!!! Tente novamente."
+		//document.getElementById("erro").innerHTML = "login ou senha incorretos!!!<br>Tente novamente.";
+	}
+	console.log(login);
+	console.log(senha);
+	//alert("teste");
+}
