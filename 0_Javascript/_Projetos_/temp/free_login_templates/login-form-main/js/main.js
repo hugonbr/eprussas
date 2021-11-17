b8e460e8c36a8743714fa7734b602a6c08095e10
Hugo Régis <hugonbr@gmail.com>
2021-11-17 20:49:15 -0300

    mod

--- 0_Javascript/_Projetos_/temp/free_login_templates/login-form-main/js/main.js
+++ 0_Javascript/_Projetos_/temp/free_login_templates/login-form-main/js/main.js
@@ -12,20 +12,17 @@ function entrar() {
 
 	login = document.getElementById("login").value;
 	senha = document.getElementById("senha").value;
+	console.log(login);
+	console.log(senha);
 
 	if (login === "admin" && senha === "123") {
 		//alert("Bem-vindo Admin");
 		document.getElementById("aviso").innerHTML =
-			"Bem-vindo!"
-		//window.open('bemvindo.html');
-		window.location.href = 'bemvindo.html';
+			"Bem-vindo! Em breve você aprenderá backend e rotas"
 	} else {
 		//alert("login ou senha incorretos!!! Tente novamente.");
 		document.getElementById("aviso").innerHTML =
 			"login ou senha incorretos!!! Tente novamente."
-		//document.getElementById("erro").innerHTML = "login ou senha incorretos!!!<br>Tente novamente.";
 	}
-	console.log(login);
-	console.log(senha);
 	//alert("teste");
 }
