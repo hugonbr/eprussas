d2555d21fff9c94fec2a427989ef1a4505a13a62
Hugo Régis <hugonbr@gmail.com>
2021-11-18 15:08:14 -0300

    mod

--- 0_Javascript/_Projetos_/1_free_login_templates/0_login-form-main/js/main.js
+++ 0_Javascript/_Projetos_/1_free_login_templates/0_login-form-main/js/main.js
@@ -39,7 +39,6 @@ function entrar() {
 			'<span class="alert alert-danger text-center">login ou senha incorretos!</span>';
 		console.log("login ou senha incorretos!");
 	}
-	return false;
 	//alert("teste");
 }
 
