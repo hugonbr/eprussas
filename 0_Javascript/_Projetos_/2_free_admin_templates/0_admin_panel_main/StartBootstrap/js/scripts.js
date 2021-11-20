c7bf85a7ffec45e35ae724c6f2033ed26332acfd
Hugo Régis <hugonbr@gmail.com>
2021-11-20 10:34:48 -0300

    mod

--- 0_Javascript/_Projetos_/2_free_admin_templates/0_admin_panel_main/StartBootstrap/js/scripts.js
+++ 0_Javascript/_Projetos_/2_free_admin_templates/0_admin_panel_main/StartBootstrap/js/scripts.js
@@ -7,6 +7,10 @@
 // Scripts
 // 
 
+let login, senha;
+
+text_button = document.getElementById("show");
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Toggle the side navigation
@@ -27,6 +31,47 @@ window.addEventListener('DOMContentLoaded', event => {
 
 //mod Hugo
 
+function entrar() {
 
+    login = document.getElementById("login").value;
+    senha = document.getElementById("senha").value;
+    console.log(login);
+    console.log(senha);
+
+    if (login == "" || senha == "") {
+        document.getElementById("aviso").innerHTML =
+            '<span class="alert alert-warning text-center">Campo(s) Vazio(s).</span>';
+        console.log("Campo(s) Vazio(s)");
+    }
+
+    // else if (login === "admin" && contTentativas == 4) {
+    // 	document.getElementById("aviso").innerHTML =
+    // 		'<span class="alert alert-danger text-center">Conta bloqueada!</span>';
+    // }
+
+    else if (login === "admin" && senha === "123") {
+        //alert("Bem-vindo Admin");
+        window.location.href = "index.html";
+        document.getElementById("aviso").innerHTML =
+            '<span class="alert alert-success text-center">Bem-vindo!</span>';
+    } else {
+        //alert("login ou senha incorretos!!! Tente novamente.");
+        document.getElementById("aviso").innerHTML =
+            '<span class="alert alert-danger text-center">login ou senha incorretos!</span>';
+        console.log("login ou senha incorretos!");
+    }
+    //alert("teste");
+}
+
+function mostrar() {
+    let isenha = document.getElementById("senha");
+    if (isenha.type === "password") {
+        isenha.type = "text";
+        text_button.textContent = "ocultar senha";
+    } else {
+        isenha.type = "password";
+        text_button.textContent = "mostrar senha";
+    }
+}
 
 //FIM mod Hugo
