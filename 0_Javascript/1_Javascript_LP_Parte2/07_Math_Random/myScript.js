704471c2aca79e21bc9e7f483fc56161c1130139
Hugo Régis <hugonbr@gmail.com>
2021-11-22 10:16:00 -0300

    mod

--- 0_Javascript/1_Javascript_LP_Parte2/07_Math_Random/myScript.js
+++ 0_Javascript/1_Javascript_LP_Parte2/07_Math_Random/myScript.js
@@ -1,20 +1,20 @@
 let min, max, r;
 
 function myFunction() {
-    min = document.getElementById("inputMin").value;
-    max = document.getElementById("inputMax").value;
+    min = document.getElementById("inputMin").value; //"x"
+    max = document.getElementById("inputMax").value; //"y"
 
-    if (min.length == "" || max.length == "") {
-        console.log("Campos vazios");
-    }
+    // if (min.length == "" || max.length == "") {
+    //     console.log("Campos vazios");
+    // }
 
-    if (min >= max || min < 0 || max < 0 || (min.length == "" || max.length == "")) {
+    if (min >= max || min < 0 || max < 0 || max >= 100 || (min.length == "" || max.length == "")) {
         document.getElementById("r").innerHTML = "Erro, dados inválidos, verifique os valores mínimo e máximo.";
         document.getElementById("r2").innerHTML = "";
     } else {
-        min = Number(min);
-        max = Number(max);
-        r = Math.floor(Math.random() * (max - min + 1)) + min;
+        min = Number(min); //x
+        max = Number(max); //y
+        r = Math.floor(Math.random() * (max - min + 1) + min);
         console.log(r);
         document.getElementById("r").innerHTML = "";
         document.getElementById("r2").innerHTML = `MIN: ${min}, MAX: ${max} -> ${r}`;
