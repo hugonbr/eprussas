9ade8871c2c60d3d104b9d721cdf114b27cfd88c
Hugo Régis <hugonbr@gmail.com>
2021-11-29 14:05:28 -0300

    mod

---
+++ 0_Javascript/0_Frameworks_/0_nodejs/1_webserver/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<p style="text-align: center;">Hello Server</p>');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
