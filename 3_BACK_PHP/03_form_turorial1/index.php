3e64bdc6f98f7dc095561c608a7d0bbc7552fe9d
Hugo Régis <hugonbr@gmail.com>
2022-01-21 15:45:57 -0300

    mod

--- 3_BACK_PHP/03_form_turorial1/index.php
+++ 3_BACK_PHP/03_form_turorial1/index.php
@@ -20,10 +20,16 @@ if (!isset($_SESSION["user"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        * {
+            text-align: center;
+        }
+    </style>
     <title>Document</title>
 </head>
 
 <body>
+    <br><br><br><br><br><br>
     <h1>WELCOME</h1>
 
     <!-- LOGOUT -->
--- 3_BACK_PHP/03_form_turorial1/login.php
+++ 3_BACK_PHP/03_form_turorial1/login.php
@@ -4,26 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        * {
+            text-align: center;
+        }
+    </style>
     <title>Document</title>
 </head>
 <body>
-
+<br><br><br><br>
 <?php
 // (A) LOGIN CHECKS
 require "check.php";
 ?>
 LOGIN PAGE HTML
-<?php if (isset($failed)) { ?>
-<div id="bad-login">Invalid user or password.</div>
-<?php } ?>
  
 <form id="login-form" method="post" target="_self">
   <h1>PLEASE SIGN IN</h1>
-  <label for="user">User</label>
-  <input type="text" name="user" required/>
-  <label for="password">Password</label>
-  <input type="password" name="password" required/>
+  <label for="user">User:</label>
+  <input type="text" name="user" required/><br><br>
+  <label for="password">Password:</label>
+  <input type="password" name="password" required/><br><br>
   <input type="submit" value="Sign In"/>
 </form>
+<br><br>
+<?php if (isset($failed)) { ?>
+<div id="bad-login">Invalid user or password.</div>
+<?php } ?>
+
 </body>
 </html>
