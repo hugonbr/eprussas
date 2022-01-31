686c9fed011cb078d70890fac4b082be8bfd4e33
Hugo Régis <hugonbr@gmail.com>
2022-01-31 10:33:18 -0300

    mod

--- 0_temp_conding/00_CSS/p0_SiteResponsivoGrid/p0 v6_web_dev/app.js
+++ 0_temp_conding/00_CSS/p0_SiteResponsivoGrid/p0 v6_web_dev/app.js
@@ -1,5 +1,6 @@
 //Get the button:
 mybutton = document.getElementById("myBtn");
+mybutton2 = document.getElementById("myBtn2");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
@@ -7,8 +8,10 @@ window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
+        mybutton2.style.display = "block";
     } else {
         mybutton.style.display = "none";
+        mybutton2.style.display = "none";
     }
 }
 
