528d7e52369e7325bc8ca1bf6dc3c4c8ef5b49b6
Hugo Régis <hugonbr@gmail.com>
2022-02-15 08:27:31 -0300

    mod

--- 6_DataBaseSQL/0_MySQL_SQL/00_olamundo/git00_temp.sql
+++ 6_DataBaseSQL/0_MySQL_SQL/00_olamundo/git00_temp.sql
@@ -1,6 +1,8 @@
-create database git_temp DEFAULT CHARACTER SET utf8;
-use git_temp;
+-- Database
+create database git00_temp DEFAULT CHARACTER SET utf8;
+use git00_temp;
 -- drop database temp;
+-- Create table
 create table usuario (
     login varchar (100) NOT NULL,
     nome varchar (150) NOT NULL,
@@ -10,6 +12,7 @@ create table usuario (
 );
 -- ENGINE = MyISAM;
 /*Adicione a ENGINE para usar MyISAM, o InnoDB é o padrão (ACID)*/
+-- Inserts
 insert into usuario
 values (
         'teste',
@@ -31,5 +34,9 @@ values (
         'teste@email.com',
         '1234'
     );
+-- Select Usuários
 select *
+from usuario;
+-- Quantidade Usuários
+select count(login)
 from usuario;
