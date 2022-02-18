37cfd92d2c8f3b78a9f9093914fd2855a92441a8
Hugo Régis <hugonbr@gmail.com>
2022-02-18 12:50:14 -0300

    mod

---
+++ 0_temp_conding/0_php_mysql/0_PDO/teste.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$db_name = "git01_userModel";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
