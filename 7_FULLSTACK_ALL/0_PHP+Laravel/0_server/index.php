eff6a2b5f078805471db34e14734a8425c3ba24b
Hugo Régis <hugonbr@gmail.com>
2022-03-02 17:04:07 -0300

    mod

---
+++ 7_FULLSTACK_ALL/0_PHP+Laravel/0_server/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Minha primeira página php</h1>
+
+<?php
+// This is a single-line comment
+# This is also a single-line comment
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+echo "Olá Mundo!\n";
+?>
+
+</body>
+</html>
