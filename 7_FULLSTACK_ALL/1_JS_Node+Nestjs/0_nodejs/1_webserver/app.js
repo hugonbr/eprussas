e11b38d6bd90ec617cd5df6d296b875bd1d31cb6
Hugo Régis <hugonbr@gmail.com>
2022-03-02 17:24:57 -0300

    mod

--- 7_FULLSTACK_ALL/1_JS_Node+Nestjs/0_nodejs/1_webserver/app.js
+++ 7_FULLSTACK_ALL/1_JS_Node+Nestjs/0_nodejs/1_webserver/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<p style="text-align: center;">Hello Server on node.js</p>');
+    res.end('<h1 style="text-align: center;">Hello Server on node.js</h1><h2 style="text-align: center;">Bem-vindo!</h2>');
 });
 
 server.listen(port, hostname, () => {
