d9d6ce382bb8e233a60e0983f1201371a15df4b9
Hugo Régis <hugonbr@gmail.com>
2022-03-05 23:57:26 -0300

    mod

---
+++ 7_FULLSTACK_ALL/1_PHP+Laravel/1_php_CRUD/2-CRUD-freecode/0-bad/index.php
@@ -0,0 +1,67 @@
+<?php
+    $pdo = new PDO ('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '1234');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+    $statement->execute();
+    $products = $statement->fetchAll(PDO::FETCH_ASSOC);
+    // echo '<pre>';
+    // var_dump($products);
+    // echo '</pre>';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="app.css">
+    <title>Products CRUD</title>
+  </head>
+  <body>
+    <h1>Products CRUD</h1>
+
+    <table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Image</th>
+        <th scope="col">Title</th>
+        <th scope="col">Price</th>
+        <th scope="col">Create Date</th>
+        <th scope="col">Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+    <!-- <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+    </tr> -->
+    <?php foreach ($products as $i => $product) { ?>
+        <tr>
+            <th scope="row"><?php echo $i + 1 ?></th>
+            <td></td>
+            <td><?php echo $product['title'] ?></td>
+            <td><?php echo $product['price'] ?></td>
+            <td><?php echo $product['create_date'] ?></td>
+            <td>
+                <button type="button" class="btn btn-sm btn-primary">Edit</button>
+                <button type="button" class="btn btn-sm btn-danger">Delete</button>
+            </td>
+            
+        </tr>
+    <?php } ?>
+    </tbody>
+    </table>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
