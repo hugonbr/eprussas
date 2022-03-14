46f43e52fb2b2887a2837eea6deb2ee273206939
Hugo Régis <hugonbr@gmail.com>
2022-03-14 11:25:07 -0300

    mod

--- 7_FULLSTACK_ALL/1_PHP+Laravel/1_php_CRUD/2-CRUD-freecode/github/03_good/Database.php
+++ 7_FULLSTACK_ALL/1_PHP+Laravel/1_php_CRUD/2-CRUD-freecode/github/03_good/Database.php
@@ -23,7 +23,7 @@ class Database
 
     public function __construct()
     {
-        $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
+        $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '1234');
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         self::$db = $this;
     }
