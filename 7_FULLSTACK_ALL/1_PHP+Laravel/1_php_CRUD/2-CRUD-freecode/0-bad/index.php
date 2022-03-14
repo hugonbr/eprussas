890077fe26bab3a4e9372023f9680648196cae60
Hugo Régis <hugonbr@gmail.com>
2022-03-14 11:10:51 -0300

    mod

--- 7_FULLSTACK_ALL/1_PHP+Laravel/1_php_CRUD/2-CRUD-freecode/0-bad/index.php
+++ 7_FULLSTACK_ALL/1_PHP+Laravel/1_php_CRUD/2-CRUD-freecode/0-bad/index.php
@@ -8,6 +8,18 @@
     // echo '<pre>';
     // var_dump($products);
     // echo '</pre>';
+
+    $keyword = $_GET['search'] ?? null;
+
+    if ($keyword) {
+        $statement = $pdo->prepare('SELECT * FROM products WHERE title like :keyword ORDER BY create_date DESC');
+        $statement->bindValue(":keyword", "%$keyword%");
+    } else {
+        $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+    }
+    $statement->execute();
+    $products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 <!doctype html>
@@ -34,6 +46,14 @@
       </div>
     </div>
 
+    <form action="" method="get">
+    <div class="input-group mb-3">
+      <input type="text" name="search" class="form-control" placeholder="Search" value="<?php echo $keyword ?>">
+      <div class="input-group-append">
+        <button class="btn btn-success" type="submit">Search</button>
+      </div>
+    </div>
+    </form>
 
     <table class="table">
     <thead>
