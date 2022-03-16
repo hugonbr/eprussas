f553234eb282b52c5aa2351be3dd93743a864e84
Hugo Régis <hugonbr@gmail.com>
2022-03-16 15:57:43 -0300

    mod

--- 7_FULLSTACK_ALL/1_PHP+Laravel/temp_downloads/gym/db_connect.php
+++ 7_FULLSTACK_ALL/1_PHP+Laravel/temp_downloads/gym/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','gym_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','1234','gym_db')or die("Could not connect to mysql".mysqli_error($con));
