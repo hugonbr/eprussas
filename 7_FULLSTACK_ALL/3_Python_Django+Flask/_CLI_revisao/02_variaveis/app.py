adb5048b7b86b3322968b2499de21d86a03227b8
Hugo Régis <hugonbr@gmail.com>
2022-03-28 11:38:07 -0300

    mod

--- 7_FULLSTACK_ALL/3_Python_Django+Flask/_CLI_revisao/02_variaveis/app.py
+++ 7_FULLSTACK_ALL/3_Python_Django+Flask/_CLI_revisao/02_variaveis/app.py
@@ -5,8 +5,9 @@ apenas para organizar alguns comandos
 '''
 x = 1
 y = 2
-
+a, b = 10, 20
 
 
 
 print (f'o valor de x é {x} e o valor de y é {y}')
+print (f'o valor de a é {a} e o valor de b é {b}')
---
+++ 7_FULLSTACK_ALL/3_Python_Django+Flask/_CLI_revisao/03_entradaDeDados/app.py
@@ -0,0 +1,4 @@
+nome = input('Digite o seu nome: ')
+
+print(f'O nome digitado foi {nome}.')
+
