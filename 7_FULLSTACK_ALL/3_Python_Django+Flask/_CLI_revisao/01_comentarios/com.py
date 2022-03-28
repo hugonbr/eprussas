5719cb8bb27d3e5b043628e0b29a52ed405782a9
Hugo Régis <hugonbr@gmail.com>
2022-03-28 11:21:33 -0300

    mod

---
+++ 7_FULLSTACK_ALL/3_Python_Django+Flask/_CLI_revisao/01_comentarios/com.py
@@ -0,0 +1,13 @@
+#Isso é um comentário
+
+'''
+Isso também é um comentário,
+só que em várias linhas...
+
+
+Então eu posso escrever a vontade aqui
+nas aspas duplas
+'''
+
+print ('Todo comentário é ignorado pelo interpretador python.')
+
---
+++ 7_FULLSTACK_ALL/3_Python_Django+Flask/_CLI_revisao/02_variaveis/vars.py
@@ -0,0 +1,12 @@
+#Declaração das variáveis
+'''
+Lembrando que as linhas em branco são ignoradas, servem
+apenas para organizar alguns comandos
+'''
+x = 1
+y = 2
+
+
+
+
+print (f'o valor de x é {x} e o valor de y é {y}')
