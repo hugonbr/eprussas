36be65aa1a4c3c39b0118f576c68c8d13e6939f9
Hugo Régis <hugonbr@gmail.com>
2022-03-29 07:41:53 -0300

    mod

---
+++ 7_FULLSTACK_ALL/3_Python_Django+Flask/_CLI_revisao/05_operacoes/app.py
@@ -0,0 +1,9 @@
+# n1 = float(input('Digite o n1:'))
+# n2 = float(input('Digite o n2:'))
+
+n1, n2 = 3, 2
+
+print(f'n1+n2={n1+n2}')
+print(f'n1-n2={n1-n2}')
+print(f'n1*n2={n1*n2}')
+print(f'n1/n2={n1/n2}')
