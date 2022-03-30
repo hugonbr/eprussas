cd34a59db23ff8345d44f0fae7df7292654ed448
Hugo Régis <hugonbr@gmail.com>
2022-03-30 16:03:19 -0300

    mod

---
+++ 7_FULLSTACK_ALL/3_Python_Django+Flask/_CLI_revisao/08_forLoop/app.py
@@ -0,0 +1,10 @@
+# frutas = ["maçã", "banana", "cereja"]
+# for fruta in frutas:
+#   print(fruta)
+
+frutas = ["maçã", "banana", "cereja"]
+for fruta in frutas:
+    print(3*"_")
+    for l in fruta:
+        print(f'|{l}|')
+    print(3*"_")
---
+++ 7_FULLSTACK_ALL/3_Python_Django+Flask/_CLI_revisao/09_range/app.py
@@ -0,0 +1,13 @@
+for x in range(1,4):
+    print(f'{x}°')
+print(10*'-')
+for x in range(0,11,2):
+    # if (x<10):
+    #     print(f'|0{x} é par|')
+    # else:
+    #     print(f'|{x} é par|')
+    if (x<10):
+        x = str(x)
+        x = '0'+x
+    print(f'|{x} é par|')
+print(10*'-')
