1d10aef7d41349274f0c115d701503bd7c56cc86
Hugo Régis <hugonbr@gmail.com>
2022-04-01 11:12:54 -0300

    mod

---
+++ 7_FULLSTACK_ALL/3_Python_Django+Flask/_CLI_revisao/17_modulo/app.py
@@ -0,0 +1,11 @@
+# importando tudo do arquivo soma
+from mat.soma import *
+# importando somente a fun soma
+# fazendo isso o app não reconhece a variável pi
+#from mat.soma import soma
+
+print(soma(2, 3))
+print(soma(1, 3))
+print(soma(10, 3))
+
+print(pi)
---
+++ 7_FULLSTACK_ALL/3_Python_Django+Flask/_CLI_revisao/17_modulo/mat/soma.py
@@ -0,0 +1,4 @@
+pi = 3.14159
+
+def soma (a, b):
+    return a+b;
