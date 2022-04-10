05e4be6cd50141fd0e96501eb710bbae78aee6e9
Hugo Régis <hugonbr@gmail.com>
2022-04-10 18:27:23 -0300

    mod

--- 7_FULLSTACK_ALL/2_Python_Django+Flask/WEB-1_Flask/01_form/app.py
+++ 7_FULLSTACK_ALL/2_Python_Django+Flask/WEB-1_Flask/01_form/app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', messages=messages)
 
 
 app.run(host='localhost', port=7000)
