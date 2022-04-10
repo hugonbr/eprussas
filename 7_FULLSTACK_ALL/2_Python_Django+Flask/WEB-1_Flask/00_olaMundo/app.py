a770b11bda3c911cf9b96a92ed8a07a8f9d72147
Hugo Régis <hugonbr@gmail.com>
2022-04-10 17:51:11 -0300

    mod

--- 7_FULLSTACK_ALL/2_Python_Django+Flask/WEB-1_Flask/00_olaMundo/app.py
+++ 7_FULLSTACK_ALL/2_Python_Django+Flask/WEB-1_Flask/00_olaMundo/app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 app.run(host='localhost', port=7000)
 
