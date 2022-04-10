e79368ef09895aabdcc80ed0c06b0c12edd0ebed
Hugo Régis <hugonbr@gmail.com>
2022-04-10 18:35:20 -0300

    mod

--- 7_FULLSTACK_ALL/2_Python_Django+Flask/WEB-1_Flask/01_form/app.py
+++ 7_FULLSTACK_ALL/2_Python_Django+Flask/WEB-1_Flask/01_form/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0c69dea4f2466a82d1652ca19369364e38e0acea0b01c007'
 
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
@@ -13,5 +15,20 @@ messages = [{'title': 'Message One',
 def index():
     return render_template('index.html', messages=messages)
 
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+    
+    return render_template('create.html')
 
 app.run(host='localhost', port=7000)
