f9d42defc8ca09213e196c3ec0fe4cc679d80e2a
Hugo Régis <hugonbr@gmail.com>
2022-04-10 14:56:31 -0300

    mod

--- 7_FULLSTACK_ALL/2_Python_Django+Flask/CLI_terminal/DATABASE/MySQL/04_Login_CRUD/app.py
+++ 7_FULLSTACK_ALL/2_Python_Django+Flask/CLI_terminal/DATABASE/MySQL/04_Login_CRUD/app.py
@@ -1,6 +1,5 @@
-op = 1
-
 import mysql.connector
+op = 1
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -11,17 +10,18 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-while op != 3:
-    print("----SIS MENU----")
+while op != 4:
+    print("----SIS CRUD----")
     print("Digite uma opção:")
     print("1- Entrar")
     print("2- Cadastrar")
-    print("3- Sair")
+    print("3- Listar usuários")
+    print("4- Sair")
     op = int(input())
     if op == 1:
         print("Digite login e senha")
         erro = True
-        while (erro): 
+        while (erro):
             #
             login = input("login: ")
             senha = input("senha: ")
@@ -32,30 +32,38 @@ while op != 3:
             mycursor.execute(sql, val)
             usuarioR = mycursor.fetchone()
 
-            if usuarioR == None: 
+            if usuarioR == None:
                 print("Erro, tente novamente...")
             else:
                 erro = False
-                op = 1
+                opU = 1
                 print(f'Olá {usuarioR[3]}, Bem-vindo!')
-                
-                while op != 4:
+
+                while opU != 4:
                     print("O que deseja fazer?")
                     print("1 - Ver meus dados")
                     print("2 - Atualizar meus dados")
                     print("3 - Deletar minha conta")
                     print("4 - Sair")
-                    
-                    op = int(input())
-                    
-                    if op == 1:
+
+                    opU = int(input())
+
+                    if opU == 1:
                         print(f'Seus dados: {usuarioR}')
                         print(f'Tipo de dado: {type(usuarioR)}')
-                    elif op == 2:
+                    elif opU == 2:
                         print("Atualizando...")
-                    elif op == 3:
+
+                    elif opU == 3:
                         print("Deletando...")
-                    elif op == 4:
+
+                        sql = f'DELETE FROM usuario WHERE id = {usuarioR[0]}'
+                        mycursor.execute(sql)
+                        mydb.commit()
+                        print("Conta deletada!")
+                        break
+
+                    elif opU == 4:
                         print("Saindo...")
                     else:
                         print("Opção inválida, tente novamente")
@@ -63,11 +71,11 @@ while op != 3:
             #
     elif op == 2:
         print("Cadastre-se abaixo:")
-        
+
         login = input("login: ")
         senha = input("senha: ")
         nome = input("nome: ")
-        
+
         sql = "INSERT INTO usuario (login, senha, nome) VALUES (%s, %s, %s)"
         val = (login, senha, nome)
         mycursor.execute(sql, val)
@@ -75,8 +83,11 @@ while op != 3:
         mydb.commit()
 
         print("Usuário cadastrado, ID:", mycursor.lastrowid)
-        
+
     elif op == 3:
+        print("Lista de Usuários: ")
+        print("...")
+    elif op == 4:
         print("Bye")
     else:
         print("Opção inválida, tente novamente")
