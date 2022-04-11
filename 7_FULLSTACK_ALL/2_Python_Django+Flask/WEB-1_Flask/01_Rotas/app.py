ecb41d8d7166f2f9ed82ae65f82734e0b868678e
Hugo Régis <hugonbr@gmail.com>
2022-04-11 18:23:09 -0300

    mod

--- 7_FULLSTACK_ALL/2_Python_Django+Flask/WEB-1_Flask/01_Rotas/app.py
+++ 7_FULLSTACK_ALL/2_Python_Django+Flask/WEB-1_Flask/01_Rotas/app.py
@@ -13,10 +13,4 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/sys')
-def sys():
-    login = request.args.get("login", "null")
-    return render_template('sys.html', login=login)
-
-
 app.run(host='localhost', port=7000)
