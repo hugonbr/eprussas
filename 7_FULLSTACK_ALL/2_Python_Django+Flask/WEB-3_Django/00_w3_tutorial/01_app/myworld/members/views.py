6db2e9a9a2b1f3dc823cde7364395a0c4622e691
Hugo Régis <hugonbr@gmail.com>
2022-04-12 08:06:07 -0300

    mod

--- 7_FULLSTACK_ALL/2_Python_Django+Flask/WEB-3_Django/00_w3_tutorial/01_app/myworld/members/views.py
+++ 7_FULLSTACK_ALL/2_Python_Django+Flask/WEB-3_Django/00_w3_tutorial/01_app/myworld/members/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def index(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
