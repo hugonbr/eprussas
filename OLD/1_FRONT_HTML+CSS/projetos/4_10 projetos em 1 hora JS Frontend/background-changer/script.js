5c4dc9ca822e425430375fac8b541992325d80a5
Hugo Régis <hugonbr@gmail.com>
2022-04-17 11:27:44 -0300

    mod

--- OLD/1_FRONT_HTML+CSS/projetos/4_10 projetos em 1 hora JS Frontend/background-changer/script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", () => {
-    document.body.style.background = randomBg();
-});
-
-function randomBg() {
-    return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
-}
--- OLD/1_FRONT_HTML+CSS/projetos/4_10 projetos em 1 hora JS Frontend/dark-mode-toggle/script.js
+++
@@ -1,5 +0,0 @@
-const toggle = document.getElementById("toggle");
-
-toggle.addEventListener("change", (e) => {
-    document.body.classList.toggle("dark", e.target.checked);
-});
