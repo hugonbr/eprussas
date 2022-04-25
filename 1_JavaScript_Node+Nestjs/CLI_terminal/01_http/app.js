0133606828f4beece6bbb00d3d31e20f43665999
Hugo Régis <hugonbr@gmail.com>
2022-04-25 10:28:42 -0300

    mod

--- 1_JavaScript_Node+Nestjs/CLI_terminal/01_http/app.js
+++ 1_JavaScript_Node+Nestjs/CLI_terminal/01_http/app.js
@@ -3,9 +3,9 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Oi Mundo!');
-}).listen(8080);
+}).listen(5000);
 
 // acesse:
-// http://localhost:8080/
+// http://localhost:5000/
 // E veja o 'Oi Mundo!'
 // OBS: Olá não reconhecerá o acento
