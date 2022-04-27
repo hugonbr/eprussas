d7a174143a7f168dc1e68649a90cb8e653831912
Hugo Régis <hugonbr@gmail.com>
2022-04-27 10:09:57 -0300

    mod

--- 1_JavaScript_Node+Nestjs/z_projetos_z/00_TodoApp_(CRUD)/01_MERN/todo-app/server/db.js
+++ 1_JavaScript_Node+Nestjs/z_projetos_z/00_TodoApp_(CRUD)/01_MERN/todo-app/server/db.js
@@ -8,11 +8,11 @@ module.exports = async () => {
             useUnifiedTopology: true
         };
         await mongoose.connect(
-            "mongodb://localhost/todo-app",
-            connectionParams
+            "mongodb://127.0.0.1:27017/todo-app"
+            // mongodb://localhost:27017/myapp
         );
         console.log("Connected to database.");
     } catch (error) {
-        console.log("Could not connect to database.");
+        console.log("Could not connect to database.", error);
     }
 }
--- 1_JavaScript_Node+Nestjs/z_projetos_z/00_TodoApp_(CRUD)/01_MERN/todo-app/server/index.js
+++ 1_JavaScript_Node+Nestjs/z_projetos_z/00_TodoApp_(CRUD)/01_MERN/todo-app/server/index.js
@@ -1,3 +1,4 @@
+const tasks = require("./routes/tasks");
 const connection = require('./db');
 const cors = require("cors");
 const express = require("express");
@@ -7,6 +8,8 @@ connection();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/tasks", tasks);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
