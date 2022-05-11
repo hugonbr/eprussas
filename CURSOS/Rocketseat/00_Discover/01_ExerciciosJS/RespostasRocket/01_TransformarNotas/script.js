0116d70452f28f7331504536fc72e1b8730f0c45
Hugo Régis <hugonbr@gmail.com>
2022-05-11 11:45:49 -0300

    mod

---
+++ CURSOS/Rocketseat/00_Discover/01_ExerciciosJS/RespostasRocket/01_TransformarNotas/script.js
@@ -0,0 +1,38 @@
+let score = -453
+
+function getScore(score) {
+    let scoreA = score >= 90 && score <= 100
+    let scoreB = score >= 80 && score <= 89
+    let scoreC = score >= 70 && score <= 79
+    let scoreD = score >= 60 && score <= 69
+    let scoreF = score < 60 && score >= 0
+
+    let scoreFinal
+
+    if (scoreA) {
+        scoreFinal = "A"
+    } else if (scoreB) {
+        scoreFinal = "B"
+    } else if (scoreC) {
+        scoreFinal = "C"
+    } else if (scoreD) {
+        scoreFinal = "D"
+    } else if (scoreF) {
+        scoreFinal = "F"
+    } else {
+        scoreFinal = "Nota inválida"
+    }
+
+    return scoreFinal
+}
+
+console.log(getScore(101))
+console.log(getScore(-1))
+console.log(getScore(0))
+console.log(getScore(1))
+console.log(getScore(45))
+console.log(getScore(60))
+console.log(getScore(61))
+console.log(getScore(75))
+console.log(getScore(85))
+console.log(getScore(95))
