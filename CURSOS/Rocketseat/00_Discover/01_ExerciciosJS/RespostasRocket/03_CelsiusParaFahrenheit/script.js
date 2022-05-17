f5ea8f708270434d0100e31db2e1e0298fe8930d
Hugo Régis <hugonbr@gmail.com>
2022-05-17 11:02:13 -0300

    mod

--- CURSOS/Rocketseat/00_Discover/01_ExerciciosJS/RespostasRocket/03_CelsiusParaFahrenheit/script.js
+++ CURSOS/Rocketseat/00_Discover/01_ExerciciosJS/RespostasRocket/03_CelsiusParaFahrenheit/script.js
@@ -1,1 +1,29 @@
-console.log("teste");
+function transformDegree(degree) {
+    const celsiusExists = degree.toUpperCase().includes('C')
+    const fahrenheitExists = degree.toUpperCase().includes('F')
+
+    if (!celsiusExists && !fahrenheitExists) {
+        throw new Error('Grau não identificado')
+    }
+
+    let updatedDegree = Number(degree.toUpperCase().replace("F", ""))
+    let formula = (fahrenheit) => (fahrenheit - 32) * 5 / 9
+    let degreesign = 'C'
+
+    if (celsiusExists) {
+        updatedDegree = Number(degree.toUpperCase().replace("C", ""));
+        formula = celsius => celsius * 9 / 5 + 32
+        degreesign = 'F'
+    }
+
+    return formula(updatedDegree) + degreesign
+}
+
+try {
+    console.log(transformDegree('10C'))
+    console.log(transformDegree('50F'))
+    console.log(transformDegree('50K'))
+} catch (error) {
+    console.log(error)
+}
+
