1eb5a54dedf77d0c1116ca54dec5fb1b59614c0c
Hugo Régis <hugonbr@gmail.com>
2022-06-14 13:34:08 -0300

    mod

---
+++ CURSOS/Rocketseat/00_Discover/05_NodeApps/01_BestMeApp/index.js
@@ -0,0 +1,12 @@
+const questions = [
+    "O que aprendi hoje?",
+    "O que me deixou aborrecido? E o que eu poderia fazer para melhorar?",
+    "O que me deixou feliz hoje?",
+    "Quantas pessoas ajudei hoje?",
+]
+
+const ask = (index = 0) => {
+    process.stdout.write()
+
+    // continua
+}
---
+++ CURSOS/Rocketseat/00_Discover/05_NodeApps/01_BestMeApp/temp/test.js
@@ -0,0 +1,9 @@
+// o console.log() vai usar o comando abaixo para
+// mostrar a saída no terminal
+
+const test = 'string test';
+
+process.stdout.write("Alguma coisa\n")
+
+process.stdout.write(test + '\n')
+
