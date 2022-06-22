a49e79b9a2772defd16c64fa5a6618b9cb68bd8b
Hugo Régis <hugonbr@gmail.com>
2022-06-22 14:47:36 -0300

    mod

--- 00_projects/AULAS_DE_TI/01_pitu/backend/dist/controllers/links.js
+++ 00_projects/AULAS_DE_TI/01_pitu/backend/dist/controllers/links.js
@@ -1,7 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const links = [];
+let proxId = 1;
+function generateCode() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 5; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
 function postLink(req, res) {
-    res.send('postLink');
+    const link = req.body;
+    link.id = proxId++;
+    link.code = generateCode();
+    link.hits = 0;
+    links.push(link);
+    // res.send('postLink');
+    res.status(201).json(link);
 }
 function getLink(req, res) {
     res.send('getLink');
--- 00_projects/AULAS_DE_TI/01_pitu/backend/src/controllers/links.ts
+++ 00_projects/AULAS_DE_TI/01_pitu/backend/src/controllers/links.ts
@@ -4,11 +4,23 @@ import {Link} from '../models/link';
 const links : Link[] = [];
 let proxId = 1;
 
+function generateCode() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i=0; i<5; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
 function postLink(req: Request, res: Response) {
     const link = req.body as Link;
     link.id = proxId++;
-    
-    res.send('postLink');
+    link.code = generateCode();
+    link.hits = 0;
+    links.push(link);
+    // res.send('postLink');
+    res.status(201).json(link);
 }
 function getLink(req: Request, res: Response) {
     res.send('getLink');
