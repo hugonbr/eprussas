9a6ae7f3e9ee595e8bfea0541744745ab3b97d29
Hugo Régis <hugonbr@gmail.com>
2022-08-24 09:24:52 -0300

    mod

--- 0_logica_DS_2022/python/03_aula/formatStringPrint.py
+++ 0_logica_DS_2022/python/03_aula/formatStringPrint.py
@@ -8,3 +8,5 @@ print("João tirou 9 em lógica de programação")
 # print("y tirou x em lógica de programação")
 print(y, "tirou", x, "em lógica de programação")
 print(f'{y} tirou {x} em lógica de programação')
+
+# ----------------------------------------------- #
