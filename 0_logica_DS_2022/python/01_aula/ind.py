503444f7400d7b8e84f62b93923d941feaa4923d
Hugo Régis <hugonbr@gmail.com>
2022-09-05 09:38:32 -0300

    mod

--- 0_logica_DS_2022/python/01_aula/ind.py
+++ 0_logica_DS_2022/python/01_aula/ind.py
@@ -1,3 +1,4 @@
 if 5 > 2:
     print("Five is greater than two!")
     print("Faça isso também")
+    print("fim de algoritmo")
---
+++ 0_logica_DS_2022/python/teste_felipe/func.py
@@ -0,0 +1,8 @@
+# f(x) = x + 1
+# f(2) = x + 1 # 3
+
+def f(x):
+    return x + 1
+
+x = int(input("Digite o valor de x: "))
+print(f'f({x})={f(x)}')
