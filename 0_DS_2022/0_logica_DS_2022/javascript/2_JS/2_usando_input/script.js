8326b09aee102a8b94241649fd5245555229dd64
Hugo Régis <hugonbr@gmail.com>
2022-10-19 11:17:00 -0300

    mod

---
+++ 0_DS_2022/0_logica_DS_2022/javascript/2_JS/2_usando_input/script.js
@@ -0,0 +1,6 @@
+let valor_input;
+
+function ok() {
+    valor_input = document.getElementById("input-texto").value;
+    document.getElementById("resposta").innerHTML = valor_input;
+}
--- 2_JavaScript_Node+Nestjs/GUI_Browser/00_LogicaDeProgramacao/1_Javascript_P1_Intro/04_Forms/myScript.js
+++ 2_JavaScript_Node+Nestjs/GUI_Browser/00_LogicaDeProgramacao/1_Javascript_P1_Intro/04_Forms/myScript.js
@@ -1,14 +1,11 @@
-//Conteúdo de Estudo:
-//Funções em js para ações de botões
-//Input value
-//Variáveis
-
+// let é usado para criar uma variável, no caso abaixo, o nome da variável é 'x'.
 let x;
 
 function ok() {
-    // Pega o valor do input field com id="input1"
-    x = document.getElementById("input1").value; //Hugo
+    // Pega o valor do input com id="input1" e passa esse valor para x.
+    x = document.getElementById("input1").value;
+    // Injeta no id="p2" a concatenação da string com a variável x.
     document.getElementById("p2").innerHTML = "Você digitou: " + x;
-    //document.getElementById("p2").innerHTML = `Valor do input: ${x}`;
-
+    // Aqui também injeta no id="p2", porém, com uma string formatada pelo javascript.
+    document.getElementById("p2").innerHTML = `Valor do input: ${x}`;
 }
