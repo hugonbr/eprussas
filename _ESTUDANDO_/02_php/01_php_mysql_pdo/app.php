270760a8fa9e816d3f021c676ca5a30b347bf806
Hugo Régis <hugonbr@gmail.com>
2023-02-16 16:34:55 -0300

    mod

--- _ESTUDANDO_/02_php/01_php_mysql_pdo/app.php
+++ _ESTUDANDO_/02_php/01_php_mysql_pdo/app.php
@@ -9,46 +9,51 @@
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
 <body>
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "1234";
-    $dbname = "ESTUDANDO_TODOLIST";
 
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Conexão realizada com sucesso"; 
+    <div class="container my-4">
+
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "1234";
+        $dbname = "ESTUDANDO_TODOLIST";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexão realizada com sucesso"; 
+            }
+        catch(PDOException $e)
+            {
+            echo "Falha na conexão: " . $e->getMessage();
+            }
+
+        $stmt = $conn->prepare("SELECT * FROM todo"); 
+        $stmt->execute();
+
+        // set the resulting array to associative
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+        $rows = $stmt->fetchAll();
+
+        echo "<table class=\"table table-success table-striped\">";
+        echo "<tr>";
+        echo "<th>ID</th>";
+        echo "<th>Task</th>";
+        echo "</tr>";
+
+        foreach ($rows as $row) {
+                echo "<tr>";
+                echo "<td>" . $row["id"] . "</td>";
+                echo "<td>" . $row["task"] . "</td>";
+                echo "</tr>";
         }
-    catch(PDOException $e)
-        {
-        echo "Falha na conexão: " . $e->getMessage();
-        }
+        echo "</table>";
 
-    $stmt = $conn->prepare("SELECT * FROM todo"); 
-    $stmt->execute();
+        $conn = null;
+        ?>
 
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-    $rows = $stmt->fetchAll();
-
-    echo "<table class=\"table table-success table-striped\">";
-    echo "<tr>";
-    echo "<th>ID</th>";
-    echo "<th>Task</th>";
-    echo "</tr>";
-
-    foreach ($rows as $row) {
-            echo "<tr>";
-            echo "<td>" . $row["id"] . "</td>";
-            echo "<td>" . $row["task"] . "</td>";
-            echo "</tr>";
-    }
-    echo "</table>";
-
-    $conn = null;
-    ?>
+    </div>
 
 </body>
 </html>
