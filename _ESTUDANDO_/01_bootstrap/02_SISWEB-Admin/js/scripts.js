d9f7e86e0f251184e2c91c2db15d9c086912a30b
Hugo Régis <hugonbr@gmail.com>
2023-03-05 13:51:44 -0300

    mod

---
+++ _ESTUDANDO_/01_bootstrap/02_SISWEB-Admin/js/scripts.js
@@ -0,0 +1,102 @@
+/*!
+    * Start Bootstrap - SB Admin v7.0.4 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2021 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+// 
+// Scripts
+// 
+
+let login, senha;
+let elem = document.documentElement;
+
+
+text_button = document.getElementById("show");
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+//mod Hugo
+
+function entrar() {
+
+    login = document.getElementById("login").value;
+    senha = document.getElementById("senha").value;
+    console.log(login);
+    console.log(senha);
+
+    if (login == "" || senha == "") {
+        document.getElementById("aviso").innerHTML =
+            '<span class="alert alert-warning text-center">Campo(s) Vazio(s).</span>';
+        console.log("Campo(s) Vazio(s)");
+    }
+
+    // else if (login === "admin" && contTentativas == 4) {
+    // 	document.getElementById("aviso").innerHTML =
+    // 		'<span class="alert alert-danger text-center">Conta bloqueada!</span>';
+    // }
+
+    else if (login === "admin" && senha === "123") {
+        //alert("Bem-vindo Admin");
+        window.location.href = "../index.html";
+        document.getElementById("aviso").innerHTML =
+            '<span class="alert alert-success text-center">Bem-vindo!</span>';
+    } else {
+        //alert("login ou senha incorretos!!! Tente novamente.");
+        document.getElementById("aviso").innerHTML =
+            '<span class="alert alert-danger text-center">login ou senha incorretos!</span>';
+        console.log("login ou senha incorretos!");
+    }
+    //alert("teste");
+}
+
+function mostrar() {
+    let isenha = document.getElementById("senha");
+    if (isenha.type === "password") {
+        isenha.type = "text";
+        text_button.textContent = "ocultar senha";
+    } else {
+        isenha.type = "password";
+        text_button.textContent = "mostrar senha";
+    }
+}
+
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+}
+
+function testeForm() {
+    alert("ok");
+}
+//FIM mod Hugo
