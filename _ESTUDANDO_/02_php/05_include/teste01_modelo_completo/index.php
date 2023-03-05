83d5cef34f42ef95947ed464e52abd6cbf181371
Hugo Régis <hugonbr@gmail.com>
2023-03-05 14:14:32 -0300

    mod

---
+++ _ESTUDANDO_/02_php/05_include/teste01_modelo_completo/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="p-5 bg-primary text-white text-center">
+  <h1>My First Bootstrap 5 Page</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="p-5 bg-primary text-white text-center">
+  <h1>Conteúdo</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia commodi esse totam nobis mollitia temporibus numquam vero obcaecati tenetur, voluptas maxime modi aspernatur molestias dolorem doloribus dolores nihil necessitatibus id.</p> 
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia commodi esse totam nobis mollitia temporibus numquam vero obcaecati tenetur, voluptas maxime modi aspernatur molestias dolorem doloribus dolores nihil necessitatibus id.</p> 
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia commodi esse totam nobis mollitia temporibus numquam vero obcaecati tenetur, voluptas maxime modi aspernatur molestias dolorem doloribus dolores nihil necessitatibus id.</p> 
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia commodi esse totam nobis mollitia temporibus numquam vero obcaecati tenetur, voluptas maxime modi aspernatur molestias dolorem doloribus dolores nihil necessitatibus id.</p> 
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia commodi esse totam nobis mollitia temporibus numquam vero obcaecati tenetur, voluptas maxime modi aspernatur molestias dolorem doloribus dolores nihil necessitatibus id.</p>
+</div>
+
+<div class="p-4 bg-dark text-white text-center">
+  <p>Footer</p>
+</div>
+
+</body>
+</html>
