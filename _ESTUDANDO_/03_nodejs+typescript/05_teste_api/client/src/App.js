8494e720d1fae21141a2df026355ce9778d8221a
Hugo Régis <hugonbr@gmail.com>
2023-03-15 11:57:42 -0300

    mod

---
+++ _ESTUDANDO_/03_nodejs+typescript/05_teste_api/client/src/App.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:5000/api');
+      setMessage(response.data.message);
+    };
+
+    fetchData().then(() => {
+      // Indicate that the effect can be deferred
+      // until after the main thread is idle
+      if (typeof window !== 'undefined' && window?.React?.startTransition) {
+        window.React.startTransition(() => {});
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ _ESTUDANDO_/03_nodejs+typescript/05_teste_api/simple-api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello, World! Its works :)' });
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
