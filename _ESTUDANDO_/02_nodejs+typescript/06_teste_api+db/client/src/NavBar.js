316a655a77b51c017518d63266115e5e01d3971a
Hugo Régis <hugonbr@gmail.com>
2023-04-05 17:15:14 -0300

    mod

--- _ESTUDANDO_/02_nodejs+typescript/06_teste_api+db/client/src/NavBar.js
+++ _ESTUDANDO_/02_nodejs+typescript/06_teste_api+db/client/src/NavBar.js
@@ -11,13 +11,13 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
+              {/* <a className="nav-link" href="#">About</a> */}
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
+              {/* <a className="nav-link" href="#">Contact</a> */}
             </li>
           </ul>
         </div>
