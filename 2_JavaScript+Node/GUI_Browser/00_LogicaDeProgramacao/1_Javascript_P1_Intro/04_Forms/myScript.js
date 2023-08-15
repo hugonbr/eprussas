1c1583fd55730b43bfbe6d0bab161088c7b9e950
Hugo Régis <hugonbr@gmail.com>
2023-08-15 17:02:08 -0300

    mod

--- 2_JavaScript+Node/GUI_Browser/00_LogicaDeProgramacao/1_Javascript_P1_Intro/04_Forms/myScript.js
+++ 2_JavaScript+Node/GUI_Browser/00_LogicaDeProgramacao/1_Javascript_P1_Intro/04_Forms/myScript.js
@@ -5,7 +5,7 @@ function ok() {
     // Pega o valor do input com id="input1" e passa esse valor para x.
     x = document.getElementById("entrada").value;
     // Injeta no id="p2" a concatenação da string com a variável x.
-    document.getElementById("saida").innerHTML = "Você digitou: " + x;
+    document.getElementById("saida1").innerHTML = "Você digitou: " + x;
     // Aqui também injeta no id="p2", porém, com uma string formatada pelo javascript.
-    document.getElementById("saida").innerHTML = `Valor do input: ${x}`;
+    document.getElementById("saida2").innerHTML = `Você digitou: ${x}`;
 }
